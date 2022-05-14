ac2319fc29d04c59539a93c8dcbf8911dd3b80ea
MHervian <mhervian0@gmail.com>
2022-05-14 23:17:02 +0700

    Adding some information in dashboard page

--- app/Views/v_dashboard.php
+++ app/Views/v_dashboard.php
@@ -52,13 +52,215 @@
       <div class="content">
         <div class="container-fluid">
           <div class="row">
-            <div class="col-lg-12">
+            <div class="col-md-7 col-sm-7">
               <div class="card card-primary card-outline">
+                <div class="row p-3">
+                  <div class="col-md-7">
+                    <!-- For chart -->
+                    <canvas id="donutChart" style="min-height: 250px; height: 100%; max-height: 100%; max-width: 100%;"></canvas>
+                  </div>
+                  <div class="col-md-5">
+                    <div class="info-box bg-gradient-success">
+                      <span class="info-box-icon"><i class="fas fa-cash-register"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Total Kredit Terbayar</span>
+                        <span class="info-box-number text-lg">Rp41,410,000.00</span>
+                      </div>
+                      <!-- /.info-box-content -->
+                    </div>
+                    <div class="info-box bg-gradient-info">
+                      <span class="info-box-icon"><i class="far fa-credit-card"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Total Kredit Jalan</span>
+                        <span class="info-box-number text-lg">Rp23,900,000.00</span>
+                      </div>
+                      <!-- /.info-box-content -->
+                    </div>
+                    <div class="info-box bg-gradient-warning">
+                      <span class="info-box-icon"><i class="far fa-bookmark"></i></span>
+
+                      <div class="info-box-content">
+                        <span class="info-box-text">Jumlah Kredit Aktif</span>
+                        <span class="info-box-number text-lg">14</span>
+                      </div>
+                      <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                    <div class="info-box bg-gradient-info">
+                      <span class="info-box-icon"><i class="fas fa-users"></i></span>
+
+                      <div class="info-box-content">
+                        <span class="info-box-text">Jumlah Pelanggan</span>
+                        <span class="info-box-number text-lg">52</span>
+                      </div>
+                      <!-- /.info-box-content -->
+                    </div>
+                  </div>
+                </div>
+                <div class="p-3">
+                  <table class="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Nama Konsumen</th>
+                        <th>No Kontak</th>
+                        <th>Tgl Lahir</th>
+                        <th>Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>Lorem</td>
+                        <td>02039238</td>
+                        <td>7 Januari 1966</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Lorem</td>
+                        <td>02039238</td>
+                        <td>7 Januari 1966</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td>Lorem</td>
+                        <td>02039238</td>
+                        <td>7 Januari 1966</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>4</td>
+                        <td>Lorem</td>
+                        <td>02039238</td>
+                        <td>7 Januari 1966</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>5</td>
+                        <td>Lorem</td>
+                        <td>02039238</td>
+                        <td>7 Januari 1966</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
               <!-- /.card -->
             </div>
+            <div class="col-md-5 col-sm-5">
+              <div class="card card-primary card-outline">
+                <div class="p-3">
+                  <div class="small-box bg-danger">
+                    <div class="inner">
+                      <h3>7 Kredit</h3>
+
+                      <p>Mendekati Batas Tunggakan Kredit</p>
+                    </div>
+                    <div class="icon">
+                      <i class="fas fa-alarm-exclamation"></i>
+                    </div>
+                  </div>
+                  <table class="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>No SP</th>
+                        <th>Nama Pemesan</th>
+                        <th>Tgl Batas</th>
+                        <th>Durasi</th>
+                        <th>Aksi</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>7 Januari 2022</td>
+                        <td class="text-red">7 Hari</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>7 Januari 2022</td>
+                        <td class="text-red">7 Hari</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>7 Januari 2022</td>
+                        <td class="text-red">6 Hari</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>4</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>7 Januari 2022</td>
+                        <td class="text-red">4 Hari</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>5</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>7 Januari 2022</td>
+                        <td class="text-red">2 Hari</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>6</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>7 Januari 2022</td>
+                        <td class="text-red">7 Hari</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>7</td>
+                        <td>Lorem</td>
+                        <td>Lorem</td>
+                        <td>7 Januari 2022</td>
+                        <td class="text-red">3 Hari</td>
+                        <td>
+                          <a href="#">Detail</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </div>
-          <!-- /.row -->
         </div>
         <!-- /.container-fluid -->
       </div>
@@ -76,8 +278,40 @@
   <script src="<?= base_url("plugins/jquery/jquery.min.js") ?>"></script>
   <!-- Bootstrap 4 -->
   <script src="<?= base_url("plugins/bootstrap/js/bootstrap.bundle.min.js") ?>"></script>
+  <!-- Chart JS -->
+  <script src="<?= base_url("plugins/chart.js/Chart.min.js") ?>"></script>
   <!-- AdminLTE App -->
   <script src="<?= base_url("dist/js/adminlte.min.js") ?>"></script>
+  <script>
+    $(function() {
+      //-------------
+      //- DONUT CHART -
+      //-------------
+      // Get context with jQuery - using jQuery's .get() method.
+      var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+      var donutData = {
+        labels: [
+          'Kredit Terbayar',
+          'Kredit Jalan'
+        ],
+        datasets: [{
+          data: [41, 24],
+          backgroundColor: ['#28a745', '#17a2b8'],
+        }]
+      }
+      var donutOptions = {
+        maintainAspectRatio: false,
+        responsive: true,
+      }
+      //Create pie or douhnut chart
+      // You can switch between pie and douhnut using the method below.
+      new Chart(donutChartCanvas, {
+        type: 'doughnut',
+        data: donutData,
+        options: donutOptions
+      })
+    })
+  </script>
 </body>
 
 </html>
